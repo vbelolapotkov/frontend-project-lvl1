49bfa7c301c67d3079b8a9542b399edc0a198ff5
Vasily Belolapotkov <belolapotkov.v@gmail.com>
2020-08-06 14:47:25 +0300

    Implement brain-even game

--- bin/brain-even.js
+++ bin/brain-even.js
@@ -1,12 +1,51 @@
 #!/usr/bin/env node
-import { greetUser } from '../src/cli.js';
+import { greetUser, requestInput } from '../src/cli.js';
 
 function printRules() {
   console.log('Answer "yes" if the number is even, otherwise answer "no".');
 }
 
+function generateNumber(MAX_NUMBER = 100) {
+  return Math.floor(Math.random() *  MAX_NUMBER);
+}
+
+function getCorrectAnswer(number) {
+  return number % 2 === 0 ? 'yes' : 'no';
+}
+
+function askQuestion(number) {
+  console.log(`Question: ${number}`);
+  return requestInput('Your answer: ');
+}
+
+function printWrongAnswer(userAnswer, correctAnswer) {
+  console.log(`"${userAnswer}" is wrong answer ;(. Correct answer was "${correctAnswer}".`);
+}
+
+function printCorrectAnswer() {
+  console.log('Correct!');
+}
+
 function launchGame() {
-  return false;
+  const ANSWERS_TO_WIN_QTY = 3;
+  let correctAnswers = 0;
+  while ( correctAnswers < ANSWERS_TO_WIN_QTY ) {
+    const number = generateNumber();
+    const correctAnswer = getCorrectAnswer(number);
+    const userAnswer = askQuestion(number);
+    if ( userAnswer !== correctAnswer ) {
+      printWrongAnswer(userAnswer, correctAnswer);
+
+      // FAILED!
+      return false;
+    }
+
+    printCorrectAnswer();
+    correctAnswers += 1;
+  }
+
+  // VICTORY!
+  return true;
 }
 
 function congratulateUser(name = '') {
