e8a2fe869b69cde504d35eeb6030016e9895bc6b
Vasily Belolapotkov <belolapotkov.v@gmail.com>
2020-08-06 15:28:36 +0300

    Move brain-even game code to src folder

---
+++ src/brain-even.js
@@ -0,0 +1,70 @@
+import { requestInput, print } from './cli.js';
+import greetUser from './games.js';
+
+function printRules() {
+  print('Answer "yes" if the number is even, otherwise answer "no".');
+}
+
+function generateNumber(MAX_NUMBER = 100) {
+  return Math.floor(Math.random() * MAX_NUMBER);
+}
+
+function getCorrectAnswer(number) {
+  return number % 2 === 0 ? 'yes' : 'no';
+}
+
+function askQuestion(number) {
+  print(`Question: ${number}`);
+  return requestInput('Your answer: ');
+}
+
+function printWrongAnswer(userAnswer, correctAnswer) {
+  print(
+    `"${userAnswer}" is wrong answer ;(. Correct answer was "${correctAnswer}".`
+  );
+}
+
+function printCorrectAnswer() {
+  print('Correct!');
+}
+
+function launchGame() {
+  const ANSWERS_TO_WIN_QTY = 3;
+  let correctAnswers = 0;
+  while (correctAnswers < ANSWERS_TO_WIN_QTY) {
+    const number = generateNumber();
+    const correctAnswer = getCorrectAnswer(number);
+    const userAnswer = askQuestion(number);
+    if (userAnswer !== correctAnswer) {
+      printWrongAnswer(userAnswer, correctAnswer);
+
+      // FAILED!
+      return false;
+    }
+
+    printCorrectAnswer();
+    correctAnswers += 1;
+  }
+
+  // VICTORY!
+  return true;
+}
+
+function congratulateUser(name = '') {
+  print(`Congratulations, ${name}!`);
+}
+
+function letsTryAgain(name = '') {
+  print(`Let's try again, ${name}!`);
+}
+
+export default function init() {
+  const name = greetUser();
+  printRules();
+  const isWon = launchGame();
+  if (isWon) {
+    congratulateUser(name);
+  } else {
+    letsTryAgain(name);
+  }
+}
