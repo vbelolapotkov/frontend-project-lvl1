516a81356c258d2363345de5b1becd44e0569893
Vasily Belolapotkov <belolapotkov.v@gmail.com>
2020-08-09 13:55:03 +0300

    Update default range in random number generator

--- src/utils.js
+++ src/utils.js
@@ -1,5 +1,15 @@
-export function randomNumber(MAX_NUMBER = 100) {
-  return Math.floor(Math.random() * MAX_NUMBER);
+/**
+ *  Generates random integer within range.
+ *
+ * @param {Number|Array} maxOrRange - Max number or range of numbers. Range is [1, maxOrRange] when Number is passed.
+ *
+ * @returns {Number}
+ */
+export function randomNumber(maxOrRange = 100) {
+  const [rangeStart, rangeEnd] = Array.isArray(maxOrRange) ? maxOrRange : [1, maxOrRange];
+  const min = Math.min(rangeStart, rangeEnd);
+  const max = Math.max(rangeStart, rangeEnd);
+  return Math.floor(min + Math.random() * (max - min));
 }
 
 export function pickRandom(array) {
