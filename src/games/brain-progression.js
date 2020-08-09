2114c90be56e60a71728732ecc7df8bc7740e497
Vasily Belolapotkov <belolapotkov.v@gmail.com>
2020-08-09 14:50:02 +0300

    Implement brain-progression

--- src/games/brain-progression.js
+++ src/games/brain-progression.js
@@ -1,12 +1,22 @@
-import createGame from '../index.js';
+import createGame, { randomNumber } from '../index.js';
 
 function getRules() {
   return 'What number is missing in the progression?';
 }
 
 function createRound() {
-  const question = '';
-  const correctAnswer = '';
+  const SEQ_LENGTH = 10;
+  const shift = randomNumber(11);
+  const step = randomNumber([2, 11]);
+  const sequence = new Array(SEQ_LENGTH).fill(0).map((_, index) =>
+    String(index * step + shift)
+  );
+
+  // Avoid hiding first and last element.
+  const hiddenIndex = randomNumber([1, sequence.length - 1]);
+  const correctAnswer = sequence[hiddenIndex];
+  sequence[hiddenIndex] = '..';
+  const question = sequence.join(' ');
   return { question, correctAnswer };
 }
 
