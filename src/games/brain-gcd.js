7f4488389262fa4d0f0024197f754e9a87fc0d4f
Vasily Belolapotkov <belolapotkov.v@gmail.com>
2020-08-13 12:36:36 +0300

    Move game specific math functions into game modules

--- src/games/brain-gcd.js
+++ src/games/brain-gcd.js
@@ -1,6 +1,33 @@
-import { randomNumber, gcd, toString } from '../utils.js';
+import { randomNumber, toString } from '../utils.js';
 
 const description = 'Find the greatest common divisor of given numbers.';
+
+/**
+ * Calculates greatest common divisor.
+ *
+ * @param {Number} a
+ * @param {Number} b
+ *
+ * @returns {Number}
+ */
+//
+function gcd(a, b) {
+  // Make sure we deal with positive numbers.
+  const [x, y] = [a, b].map(Math.abs);
+  let [greater, currentGcd] = x > y ? [x, y] : [y, x];
+
+  // Use Euclidean algorithm to calculate GCD https://en.wikipedia.org/wiki/Euclidean_algorithm.
+  while (currentGcd > 0) {
+    const nextGcd = greater % currentGcd;
+    if (nextGcd === 0) {
+      break;
+    }
+    greater = currentGcd;
+    currentGcd = nextGcd;
+  }
+
+  return currentGcd;
+}
 
 function createRound() {
   const a = randomNumber();
--- src/games/brain-prime.js
+++ src/games/brain-prime.js
@@ -1,6 +1,27 @@
-import { randomNumber, isPrime, toString } from '../utils.js';
+import { randomNumber, toString } from '../utils.js';
 
 const description = 'Answer "yes" if given number is prime. Otherwise answer "no".';
+
+/**
+ * Checks if given number is prime.
+ *
+ * @param {Number} number
+ *
+ * @returns {Boolean} True if number is prime
+ */
+function isPrime(number) {
+  if (!Number.isInteger(number) || number < 2) {
+    return false;
+  }
+
+  for (let divisor = 2; divisor < Math.sqrt(number); divisor += 1) {
+    if (number % divisor === 0) {
+      return false;
+    }
+  }
+
+  return true;
+}
 
 function createRound() {
   const number = randomNumber();
